1d899d4bd92855130727d812461e51fd795d3d09
amanjaiswal21 <amanj2003e@gmail.com>
2023-12-21 18:42:36 +0530

    my first commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import app from "./app.js"
 import connectDB from "./db/index.js"
  
 const port=process.env.PORT ||8000;
-connectDB()   
+connectDB()    
 .then(()=>{
     app.listen(8000,()=>{
         console.log(`Server is listening on : ${port}`);
