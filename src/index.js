6bece3f8c574a8941b70175196c6e2743901d529
amanjaiswal21 <amanj2003e@gmail.com>
2023-12-22 20:02:37 +0530

    second coomit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import connectDB from "./db/index.js"
  
 const port=process.env.PORT ||8000;
 connectDB()    
-.then(()=>{
+.then(()=>{ 
     app.listen(8000,()=>{
         console.log(`Server is listening on : ${port}`);
     })
