40e79b4777a8a6c2f2d2c3aa36a9f7f5a43f2772
amanjaiswal21 <amanj2003e@gmail.com>
2024-01-23 07:25:10 +0530

    update user.controller.js and user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controoler/user.controller.js";
+import { registerUser,loginUser,logoutUser,refreshAccessToken,channgeCurrentPassword,
+    updateAccountDetails,
+    updateAvatar,
+    updateUserCoverImage,
+     } from "../controoler/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js" 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,10 +27,25 @@ router.route("/register")
 router.route("/login")
 .post(loginUser);
 
-router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/logout")
+.post(verifyJWT,logoutUser);
 
-router.route("refresh-token")
+router.route("/refresh-token")
 .post(refreshAccessToken);
+
+router.route("/channgeCurrentPassword")
+.post(channgeCurrentPassword);
+
+router.route("/updateAccountDetails")
+.post(updateAccountDetails);
+
+router.route("/updateAvatar")
+.post(updateAvatar);
+
+router.route("/updateUserCoverImage")
+.post(updateUserCoverImage);
+
+
 
 
 export default router;
